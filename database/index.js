d5729139378366db450cd2cdb28ac66d18ca9dd8
Kareem-Abdelwahed <kareem.abdelwahed.a@gmail.com>
2019-12-10 16:17:05 +0200

    fix db index braces

--- database/index.js
+++ database/index.js
@@ -218,227 +218,209 @@ module.exports = {
           callback(err, results[0]);
         }
       });
-    },
-    deleteUserProject: function(x, callback) {
-      connection.query('call deleteProject(?, ?)', x, function(err, results) {
+    }
+  },
+  //cohorts functions
+  cohorts: {
+    //get all cohorts data
+    get: function(callback) {
+      connection.query(' call getCohorts()', function(err, results) {
         if (err) {
-          console.log('Can not edit user', err);
+          console.log('Can not fetch data', err);
         } else {
-          callback(err, results[0]);
+          callback(err, results);
         }
       });
     },
-    //cohorts functions
-    cohorts: {
-      //get all cohorts data
-      get: function(callback) {
-        connection.query(' call getCohorts()', function(err, results) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      },
-      //get all users by a specified cohort
-      getCohortUsers: function(callback, cohort) {
-        connection.query(' call getCohortUsers(?)', cohort, function(
-          err,
-          results
-        ) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      }
-    },
-    //skills function
-    skills: {
-      get: function(callback) {
-        connection.query(' call getSkills()', function(err, results) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      },
-      getSkillUsers: function(callback, skill) {
-        connection.query(' call getUserBySkill(?)', skill, function(
-          err,
-          results
-        ) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      }
-    },
-    //Employment Status functions
-    empStatus: {
-      get: function(callback) {
-        connection.query(' call getEmpStatus()', function(err, results) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      },
-      getUsersByEmpStatus: function(callback, empStatus) {
-        connection.query(' call getUsersByEmpStat(?)', empStatus, function(
-          err,
-          results
-        ) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      }
-    },
-    //Projects and Portfolio Functions
-    //get all user's projects depending on userId
-    portfolio: {
-      get: function(callback, userId) {
-        connection.query(' call getProjects(?)', userId, function(
-          err,
-          results
-        ) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      },
-      //get all user's projects using any part of user name
-      getProjectsByUName: function(callback, userName) {
-        connection.query(' call getUserProjects(?)', userName, function(
-          err,
-          results
-        ) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      },
-      //get a project by its id
-      getProjectById: function(callback, id) {
-        connection.query(' call getProjectById(?)', id, function(err, results) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      }
-    },
-
-    //Posts functions
-    //get all posts
-    posts: {
-      get: function(callback) {
-        connection.query(' call getPosts()', function(err, results) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      },
-      //get all posts for a specific user
-      getPostsByUser: function(callback, userName) {
-        connection.query(' call getPostsByUser(?)', userName, function(
-          err,
-          results
-        ) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      },
-      //search posts
-      search: function(callback, query) {
-        try {
-          connection.query('call searchPost(?)', query, function(err, results) {
-            if (err) {
-              console.log('Can not fetch post', err);
-            } else {
-              callback(err, results);
-            }
-          });
-        } catch (err) {
-          console.log('err ', err);
+    //get all users by a specified cohort
+    getCohortUsers: function(callback, cohort) {
+      connection.query(' call getCohortUsers(?)', cohort, function(
+        err,
+        results
+      ) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
         }
-      },
-      // get all posts published by users in a specific user
-      getPostsByCohort: function(callback, cohort) {
-        connection.query(' call getPostsByCohort(?)', cohort, function(
-          err,
-          results
-        ) {
+      });
+    }
+  },
+  //skills function
+  skills: {
+    get: function(callback) {
+      connection.query(' call getSkills()', function(err, results) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    },
+    getSkillUsers: function(callback, skill) {
+      connection.query(' call getUserBySkill(?)', skill, function(
+        err,
+        results
+      ) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    }
+  },
+  //Employment Status functions
+  empStatus: {
+    get: function(callback) {
+      connection.query(' call getEmpStatus()', function(err, results) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    },
+    getUsersByEmpStatus: function(callback, empStatus) {
+      connection.query(' call getUsersByEmpStat(?)', empStatus, function(
+        err,
+        results
+      ) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    }
+  },
+  //Projects and Portfolio Functions
+  //get all user's projects depending on userId
+  portfolio: {
+    get: function(callback, userId) {
+      connection.query(' call getProjects(?)', userId, function(err, results) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    },
+    //get all user's projects using any part of user name
+    getProjectsByUName: function(callback, userName) {
+      connection.query(' call getUserProjects(?)', userName, function(
+        err,
+        results
+      ) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    },
+    //get a project by its id
+    getProjectById: function(callback, id) {
+      connection.query(' call getProjectById(?)', id, function(err, results) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    }
+  },
+
+  //Posts functions
+  //get all posts
+  posts: {
+    get: function(callback) {
+      connection.query(' call getPosts()', function(err, results) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    },
+    //get all posts for a specific user
+    getPostsByUser: function(callback, userName) {
+      connection.query(' call getPostsByUser(?)', userName, function(
+        err,
+        results
+      ) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    },
+    //search posts
+    search: function(callback, query) {
+      try {
+        connection.query('call searchPost(?)', query, function(err, results) {
           if (err) {
-            console.log('Can not fetch data', err);
+            console.log('Can not fetch post', err);
           } else {
             callback(err, results);
           }
         });
-      },
-      //get all posts by a post type (e.g. all text posts)
-      getPostsByType: function(callback, type) {
-        connection.query(' call getPostsByType(?)', type, function(
-          err,
-          results
-        ) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      },
-      //get all posts containing a text in their/ its body
-      getPostsByBody: function(callback, text) {
-        connection.query(' call getPostsByBody(?)', text, function(
-          err,
-          results
-        ) {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(err, results);
-          }
-        });
-      },
-      addPost: function(x, callback) {
-        //console.log(callback);
-        connection.query(' call addPost(?, ?, ?)', x, (err, results) => {
-          if (err) {
-            console.log('Can not fetch data', err);
-          } else {
-            callback(null, results[0]);
-          }
-        });
-      },
-      deletePost: function(x, callback) {
-        connection.query(' call deletePost(?)', x, function(err, results) {
-          if (err) {
-            console.log('Can not delete post', err);
-          } else {
-            callback(null, results[0]);
-          }
-        });
+      } catch (err) {
+        console.log('err ', err);
       }
+    },
+    // get all posts published by users in a specific user
+    getPostsByCohort: function(callback, cohort) {
+      connection.query(' call getPostsByCohort(?)', cohort, function(
+        err,
+        results
+      ) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    },
+    //get all posts by a post type (e.g. all text posts)
+    getPostsByType: function(callback, type) {
+      connection.query(' call getPostsByType(?)', type, function(err, results) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    },
+    //get all posts containing a text in their/ its body
+    getPostsByBody: function(callback, text) {
+      connection.query(' call getPostsByBody(?)', text, function(err, results) {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(err, results);
+        }
+      });
+    },
+    addPost: function(x, callback) {
+      //console.log(callback);
+      connection.query(' call addPost(?, ?, ?)', x, (err, results) => {
+        if (err) {
+          console.log('Can not fetch data', err);
+        } else {
+          callback(null, results[0]);
+        }
+      });
+    },
+    deletePost: function(x, callback) {
+      connection.query(' call deletePost(?)', x, function(err, results) {
+        if (err) {
+          console.log('Can not delete post', err);
+        } else {
+          callback(null, results[0]);
+        }
+      });
     }
   }
 };
