3b439e33f38cd9a2005da3ca27dd14204f80cd05
Kareem-Abdelwahed <kareem.abdelwahed.a@gmail.com>
2019-12-19 15:47:45 +0200

    delete front end

--- src/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const GET_ALL_POSTS = 'GET_ALL_POSTS';
-export const ADD_POST = 'ADD_POST';
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { allPostsReducer } from './posts';
-
-export default combineReducers({
-  allPostsReducer
-});
